07fdbd93951a0082d3a86e143903976e9ba32bb9
Coder-Arm <anuastro002@gmail.com>
2023-07-01 19:53:52 +0530

    pushing code

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let searchStr = "ncs";
+const apiKey = "AIzaSyDlS51VfwD02QVgM1QltgAUZvQoTxa1KrQ";
+const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${searchStr}&part=snippet&type=video&maxResults=20`;
+
+const container = document.getElementById("container");
+function renderVideos(data){
+    data.forEach(item => {
+        const videoLink = document.createElement("a");
+        videoLink.href = "./videoDetails.html"
+        videoLink.addEventListener("click",()=>{
+            localStorage.setItem("videoId",`${item.id.videoId}`)
+        })
+        videoLink.innerHTML = `<div class="video-card">
+        <div class="video-img"><img src=${item.snippet.thumbnails.high.url} alt="video-thumbnail"></div>
+        <div class="video-title">${item.snippet.title}</div>
+        <div class="channel-name">${item.snippet.channelTitle}</div>
+        <div class="video-views"></div>
+        <div class="published-time"></div>
+        </div>`
+        container.appendChild(videoLink);
+    });
+}
+
+
+async function fetchDetails(){
+    const response = await fetch(endpoint,{method : "GET"});
+    const result = await response.json();
+    console.log(result);
+    renderVideos(result.items);
+}
+fetchDetails();
---
+++ showVideo.js
@@ -0,0 +1,93 @@
+const video = document.getElementById("yt-video");
+const videoId = localStorage.getItem("videoId");
+video.src = `https://www.youtube.com/embed/${videoId}`;
+const apiKey = "AIzaSyDlS51VfwD02QVgM1QltgAUZvQoTxa1KrQ";
+const endpoint1 = `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${apiKey}
+`
+const videoInfo = document.getElementById("video-info"); 
+function renderData(item){
+        const titleWithStats = document.createElement("div");
+        titleWithStats.className = "title-with-stats";
+        titleWithStats.innerHTML = `
+        <div class="video-title">${item.snippet.title}</div>
+        <div class="statistics">
+        <span class="views">${item.statistics.viewCount} views</span>
+        <div class="right">
+        <div class="item"><img src="./statistics/Liked.png" alt=""><span>${item.statistics.likeCount}</span></div>
+        <div class="item">
+        <img src="./statistics/Share.png" alt="share"><span>Share</span></div>
+        <div class="item">
+        <img src="./statistics/save.png" alt="save"><span>Save</span></div></div></div>`
+        const channel = document.createElement("div");
+        channel.className = "channel"
+        channel.innerHTML=`<div class="channel-top">
+        <div class="channel-name">${item.snippet.channelTitle
+        }</div><button class="subscribe">Subscribe</button>
+         </div>
+         <div class="description">${item.snippet.description.substr(0,300)}</div>
+         `
+        //  const replyBtn = document.createElement("button");
+        //  replyBtn.innerText = "Show Replies"
+        //  replyBtn.onclick = fetchComments();
+        videoInfo.append(titleWithStats,channel);
+
+}
+   
+async function fetchVideoDetails(){
+    const response = await fetch(endpoint1);
+    const result = await response.json();
+    // console.log(result.items[0])
+    renderData(result.items[0]);
+}
+fetchVideoDetails();
+ 
+const endpoint2 = `https://www.googleapis.com/youtube/v3/commentThreads
+?key=${apiKey}&part=snippet&videoId=${videoId}&maxResults=80&order=time`;
+  const commentsContainer = document.createElement("div");
+  commentsContainer.className = "commentsBox"
+  
+ function addComments(data){
+    commentsContainer.innerHTML = `<div class="comments-top"><span>${data.length} </span>Comments</div>`
+    data.forEach(item => {
+        commentsContainer.innerHTML += `<p class="comments">${item.snippet.topLevelComment.snippet.textDisplay}</p>`
+    });
+    videoInfo.appendChild(commentsContainer);
+ }
+
+async function fetchComments(){
+  const response = await fetch(endpoint2);
+  const result = await response.json();
+//    console.log(result.items);
+    addComments(result.items);
+}
+fetchComments();
+
+const searchStr = "ncs";
+const endpoint3 = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${searchStr}&part=snippet&type=video&maxResults=10` 
+async function fetchVideos(){
+    const response = await fetch(endpoint3);
+    const result = await response.json();
+    console.log(result.items);
+    getVideos(result.items);
+}
+fetchVideos();
+
+const sideVideos = document.getElementById("side-videos");
+function getVideos(data){
+    data.forEach(item => {
+         const videoCard = document.createElement("div");
+         videoCard.className = "video-card";
+         videoCard.addEventListener("click",()=>{
+            localStorage.setItem("videoId",item.id.videoId);
+            window.open("/videoDetails.html");
+            video.src = `https://www.youtube.com/embed/${videoId}`;
+         })
+         videoCard.innerHTML = `
+         <img src=${item.snippet.thumbnails.high.url} alt="something" />
+        <div class="about-video">
+           <div class="title">${item.snippet.title}</div>
+           <div class="channel-name">${item.snippet.channelTitle}</div>
+        </div>`
+           sideVideos.appendChild(videoCard);
+    })
+}
