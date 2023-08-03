90fcb676131dc90dd31e8dd7e3eb9e84bb9bcb00
Coder-Arm <anuastro002@gmail.com>
2023-08-03 11:29:26 +0530

    improvedd searching

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 
 let searchInput = document.getElementById("searchInput");
 
+searchInput.addEventListener("keyup",()=>{
+    let inputValue = searchInput.value.toLowerCase().trim();
+       fetchDetails(inputValue);
+    })
+
 const apiKey = "AIzaSyBxo8JwdPalNYmabeOeeF5lW3YdfwcPtM8";
-const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${searchInput.value}&part=snippet&type=video&maxResults=100`;
+
 
 const container = document.getElementById("container");
 function renderVideos(data){
@@ -25,18 +30,10 @@ function renderVideos(data){
 }
 
 
-async function fetchDetails(){
+async function fetchDetails(search){
+    const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${search}&part=snippet&type=video&maxResults=100`;
     const response = await fetch(endpoint,{method : "GET"});
     const result = await response.json();
-    // console.log(result);
-      searchInput.addEventListener("keyup",()=>{
-        let inputValue = searchInput.value.toLowerCase().trim();
-       let filteredData =  result.items.filter(item => {
-           return (item.snippet.title.toLowerCase().includes(inputValue))
-        })
-        // console.log(filteredData);
-        renderVideos(filteredData);
-      })
       renderVideos(result.items);
 }
 fetchDetails();
